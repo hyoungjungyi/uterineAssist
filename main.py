36c3f8bd5b3ede9f5ccc58171b84a1e3c6d6984a
hyoungjungyi <hyoungjung.yi@gmail.com>
2025-06-17 17:10:57 +0900

    added a bunch..

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from PIL import Image
 import os
 from skimage.morphology import binary_closing
 from scipy.spatial.distance import cdist
-from scipy.ndimage import convolve
 import shutil
 from pathlib import Path
 import cv2
-from utils import make_axis_attention_map, find_largest_incircle, find_largest_incircle_attention, find_largest_inscribed_circle, merge_skeletons_and_smooth
-from pca import find_major_axis_pca, draw_pca_result
+from utils import *
+import sknw
+import networkx as nx
+from skimage.draw import line
+
 
 
 palette=[
@@ -25,122 +27,26 @@ palette=[
     
 ]
 palette += [0] * (256*3 - len(palette))
-    
+ 
     
 # 필요한 3개의 클래스에 대한 스켈레톤만 시각화
 image_dir = "/mnt/home/chaelin/hyunjung/skeleton/data/train/labels"
-output_dir = r"/mnt/home/chaelin/hyunjung/skeleton/sk/allSk"
+output_dir = r"/mnt/home/chaelin/hyunjung/skeleton/sk/thickness_comparison"
 os.makedirs(output_dir, exist_ok=True)
 images = os.listdir(image_dir)
-
-
-def fill_holes_opencv(mask):
-    mask = (mask > 0).astype(np.uint8) * 255
-    im_floodfill = mask.copy()
-    h, w = mask.shape[:2]
-    mask2 = np.zeros((h+2, w+2), np.uint8)
-    cv2.floodFill(im_floodfill, mask2, (0,0), 255)
-    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
-    filled = mask | im_floodfill_inv
-    return filled
-
-def draw_major_axis(mask):
-    filled = fill_holes_opencv(mask)
-    contours, _ = cv2.findContours(filled, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if not contours:
-        return cv2.cvtColor(filled, cv2.COLOR_GRAY2BGR)
-    cnt = max(contours, key=cv2.contourArea)
-    (x, y), (width, height), angle = cv2.fitEllipse(cnt)
-    major_axis = max(width, height)
-    radius = int(major_axis / 2)
-    color_img = cv2.cvtColor(filled, cv2.COLOR_GRAY2BGR)
-    cv2.circle(color_img, (int(x), int(y)), radius, (0, 0, 255), 1)  # 빨강 원
-    return color_img
-
-def fill_mask(mask):
-
-    mask = (mask > 0).astype(np.uint8) * 255
-    mask=np.where(mask>1,1,mask)
-
-    return mask
-
-
-
-def calc_distance(pt1,pt2):
-    pt1=np.array(pt1)
-    pt2=np.array(pt2)
-    return np.linalg.norm(pt1-pt2)
-
-
-def find_endpoints(skeleton):
-    kernel = np.array([[1,1,1],
-                       [1,10,1],
-                       [1,1,1]])
-    filtered = convolve(skeleton.astype(np.uint8), kernel, mode='constant', cval=0)
-    # 중앙(10) + 이웃 1개(1) = 11인 곳이 endpoint
-    endpoints = (filtered == 11)
-    if np.sum(skeleton)==1:
-        endpoints |= skeleton.astype(bool)
-    return endpoints
-
-
-#yx xy 바꾸기
-def yx2xy(pt):
-    return (pt[1],pt[0])
-
-
-#직선 연장해서 > 마스크값이 1에서 0으로 바뀌는 지점 찾기
-def extend_line_find_transition(mask, start_point,end_point,direction='end',num_points=100):
-    """
-    start, end : y,x
-    direction: start/end 연장방향
-    num_points: 연장선 위에서 샘플링할 점 개수
-    """
-    start_point,end_point=np.array(start_point),np.array(end_point)
-    if direction=='end':
-        vector=end_point-start_point
-        base_point=end_point
-    elif direction=='start':
-        vector=start_point-end_point
-        base_point=start_point
-    else:
-        raise ValueError("direction은 start나 end만 가능합니다")
-    
-    unit_vector=vector/np.linalg.norm(vector)
-    extend_length=50
-    extended_point=base_point+unit_vector*extend_length
-    line_points=np.linspace(base_point,extended_point,num_points)
-    
-    #마스크값 변화 탐색
-    prev_value=1
-    for pt in line_points:
-        y,x = int(round(pt[0])),int(round(pt[1]))
-        if y<0 or y>=mask.shape[0] or x<0 or x>=mask.shape[1]:
-            break
-        current_value=mask[y,x]
-        if prev_value==1 and current_value==0:
-            return (y,x)
-        prev_value=current_value
-    return None
-
-
-#여러개의 점이 들어오면 가장 멀리 떨어진 두 점만 반환
-def pick_two_farthest_points(coords):
-    if len(coords) <=2 :
-        return coords
-    dists = cdist(coords,coords)
-    i,j=np.unravel_index(np.argmax(dists), dists.shape)
-    return np.array([coords[i],coords[j]])
-    
+skeleton_coords=[]
 
 
 for image_name in images:
+    cervix_end = None
+    cervix_point = None
+    endo_point = None 
     image_path = os.path.join(image_dir, image_name)
     img = np.array(Image.open(image_path))
     all_skeletons = np.zeros_like(img)
     print(f"Starting image process of {image_path}")
 
-    class_ids = [2, 3, 4]  # endo 2, cervix 3, fundus 4 클래스 번호
+    class_ids = [2, 3, 4]  # endo 2, cervix 4, fundus 3 클래스 번호
     skeletons = {}
     endpoints_dict={}
 
@@ -148,24 +54,22 @@ for image_name in images:
         mask = (img == class_id).astype(np.uint8)
         mask = binary_closing(mask, footprint=np.ones((3, 3)))
         skeleton = skeletonize(mask)
+        endpoints=find_endpoints(skeleton)
+        selected=pick_two_farthest_points(endpoints)
         skeletons[class_id] = skeleton
         all_skeletons[skeleton > 0] = class_id
-        endpoints_dict[class_id]=find_endpoints(skeleton)
+        endpoints_dict[class_id]=selected
         
     endo_skeleton = skeletons[2]
-    cervix_skeleton = skeletons[3]
-    fundus_skeleton = skeletons[4]
+    cervix_skeleton = skeletons[4]
+    fundus_skeleton = skeletons[3]
 
     #cervix 스켈레톤 너무 짧은 경우
-    if len(np.argwhere(endpoints_dict[3]))==1:
-        cervix_start=tuple(np.argwhere(endpoints_dict[3]))[0]
+    if len((endpoints_dict[4]))==1:
+        cervix_start=endpoints_dict[4][0]
     else:
-        cervix_start, cervix_end=tuple(np.argwhere(endpoints_dict[3]))[0] , tuple(np.argwhere(endpoints_dict[3]))[1]
-    #fundus 스켈레톤 너무 짧은 경우
-    if len(np.argwhere(endpoints_dict[4]))==1:
-        fundus_start=tuple(np.argwhere(endpoints_dict[4]))[0]
-    else:
-        fundus_start, fundus_end=tuple(np.argwhere(endpoints_dict[4]))[0] , tuple(np.argwhere(endpoints_dict[4]))[1]
+        farthest_points=pick_two_farthest_points(np.array(endpoints_dict[4]))
+        cervix_start, cervix_end=farthest_points[0],farthest_points[1]
 
     img_pil = Image.fromarray(img.astype(np.uint8), mode="P")
     img_pil.putpalette(palette)
@@ -180,92 +84,154 @@ for image_name in images:
     out_img.putpalette(palette)
     out_img=np.array(out_img.convert('RGB'))
     
-    #선 두개 긋기
-    mask_fundus = (img == 4).astype(np.uint8)
-    mask_cervix = (img == 3).astype(np.uint8)
+    #선 하나로 긋기 
+    """
+    선 하나 긋기 (큰 스켈레톤)
+    """
+
+    mask_fundus = (img == 3).astype(np.uint8)
+    mask_cervix = (img == 4).astype(np.uint8)
     mask_endo = (img == 2).astype(np.uint8)
     mask_uterus = fill_mask(img).astype(np.uint8)
-    transition_fundus = extend_line_find_transition(mask_fundus, fundus_start, fundus_end, direction='end')
-    transition_cervix = extend_line_find_transition(mask_cervix,cervix_start, cervix_end, direction='start')
-    print(f"transition_fundus:{transition_fundus}, transition_cervix:{transition_cervix}")
-    cv2.line(out_img,yx2xy(fundus_start),yx2xy(transition_fundus),(255,0,0),1)
-    cv2.line(out_img,yx2xy(transition_cervix),yx2xy(cervix_end),(255,0,0),1)
+    endo_coords = sorted([tuple(coord) for coord in np.argwhere(endo_skeleton)], key=lambda x: x[0])
+    cervix_coords = sorted([tuple(coord) for coord in np.argwhere(cervix_skeleton)], key=lambda x: x[0])
+    fundus_coords = sorted([tuple(coord) for coord in np.argwhere(fundus_skeleton)], key=lambda x: x[0])
+    endo_center=np.mean(endo_coords,axis=0)
+    boundary_points = []
+    connect_points = []
     
-    #길이 재기
-    length_fundus=calc_distance(fundus_start,transition_fundus)
-    length_cervix=calc_distance(transition_cervix,cervix_end)
+    if len(endpoints_dict[4]) == 1:
+        cervix_point = endpoints_dict[4][0]
+        if calc_distance(cervix_point,endpoints_dict[2][0])<=calc_distance(cervix_point,endpoints_dict[2][1]):
+            endo_point = endpoints_dict[2][0]  # endo 스켈레톤 시작
+        else:
+            endo_point = endpoints_dict[2][1]
+        base_point, direction, uterus_center = get_direction_for_short_skeleton(cervix_point, endo_point, mask_uterus)
+        boundary_points.append(cervix_point)
+        boundary_points.append(endo_point)
+    else:
+        # 기존 방식: 스켈레톤이 2픽셀 이상일 때
+        base_point, direction, uterus_center = get_direction_with_slope_and_uterus(
+            cervix_start,cervix_end, mask_uterus
+        )
+        boundary_points.append(cervix_start)
+        boundary_points.append(cervix_end)
+    if base_point is not None:
+        cervix_boundary = extend_to_boundary(mask_uterus, base_point, direction)
+        boundary_points.append(cervix_boundary)
+    # cervix_coords의 끝점에서 endo_center와 반대 방향으로 연장
+    sorted_cervix = sorted(cervix_coords, key=lambda x: x[0])
+
+
+    boundary_points = [convert_to_int_tuple(point) for point in boundary_points]
+    for point in boundary_points:
+        cv2.circle(out_img, (point[1], point[0]), 2, (255, 0, 0), -1)
+    connect_points.append(cervix_boundary)
+    endpoints_dict = reorder_endpoints_by_reference(endpoints_dict, cervix_boundary)
+    class_ids=[4,2,3]
+    for num in class_ids:
+        for point in endpoints_dict[num]:
+            connect_points.append((int(point[0]),int(point[1])))
+    skeleton_image=(all_skeletons>0).astype(np.uint8)
+            
+
     
-    #최대 내접원 찾기
-    (endo_center_x, endo_center_y), endo_diameter = find_largest_incircle(mask_endo,image_name,save_dir="endo_circle")
-    (uterus_center_x, uterus_center_y), uterus_diameter = find_largest_incircle(mask_uterus,image_name,save_dir="uterus_circle")
-    (attn_endo_center_x, attn_endo_center_y), attn_endo_diameter = find_largest_incircle_attention(mask_endo,image_name,save_dir="attn_endo_circle")
-    (attn_uterus_center_x, attn_uterus_center_y), attn_uterus_diameter = find_largest_incircle_attention(mask_uterus,image_name,save_dir="attn_uterus_circle")
-    result = find_largest_inscribed_circle(mask_endo, endo_skeleton)
-    uterus_result = find_largest_inscribed_circle(mask_uterus, combined_skeleton)
-    #pca
-    center, pc1, pc2, length, (pc1, pc2) = find_major_axis_pca(mask_uterus)
-    draw_pca_result(mask_uterus,image_name)
+    sorted_points = sort_points_greedy(connect_points)
+    print(f"connect points: {sorted_points} length of connect points: {len(sorted_points)}")
+    out_img,all_coords=custom_connect(out_img,connect_points)
+    img=np.array(img_pil.convert('RGB'))
+
     
     results=[
         f"{image_name} results",
-        "attention 없이: ",
-        f"최대 내막 내접원의 중심: ({endo_center_x}, {endo_center_y}), 지름: {endo_diameter}",
-        f"최대 uterus 내접원의 중심: ({uterus_center_x}, {uterus_center_y}), 지름: {uterus_diameter}",
-        f"스켈레톤 따라가는 최대 내접원 중심: ({result[0]}, {result[1]}), 지름: {result[2]}",
-        f"스켈레톤 따라가는 유터러스 내접원 중심: ({uterus_result[0]}, {uterus_result[1]}), 지름: {uterus_result[2]}",
-        "attention 있이:"
-        f"최대 내막 내접원의 중심: ({attn_endo_center_x}, {attn_endo_center_y}), 지름: {attn_endo_diameter}",
-        f"최대 uterus 내접원의 중심: ({attn_uterus_center_x}, {attn_uterus_center_y}), 지름: {attn_uterus_diameter}"
-        # f"pca 적용한 중심: {center}, 두께: {length}"
+        f"cervix start:{cervix_start} cervix end:{cervix_end} cervix point:{cervix_point} endo point:{endo_point}",
+        f"length of connect points: {len(connect_points)}"
     ]
+    for class_id, endpoints in endpoints_dict.items():
+        for point in endpoints:
+            cv2.circle(out_img, (int(point[1]), int(point[0])), 1, (0, 0, 255), -1)
 
+    # #최대 내접원 찾기
+    # (endo_center_x, endo_center_y), endo_diameter = find_largest_incircle(mask_endo,image_name,save_dir="endo_circle")
+    # (uterus_center_x, uterus_center_y), uterus_diameter = find_largest_incircle(mask_uterus,image_name,save_dir="uterus_circle")
+    # result = find_largest_inscribed_circle(mask_endo, endo_skeleton)
+    # uterus_result = find_largest_inscribed_circle(mask_uterus, combined_skeleton)
+    # #엔도 스켈레톤 이진화> 컨투어 추출> 길이계산
+    # endo_skeleton=(endo_skeleton>0).astype(np.uint8)
+    # contours,_=cv2.findContours(endo_skeleton,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    # if contours:
+    #     main_contour=max(contours,key=lambda x:cv2.arcLength(x, False))
+    #     length_endo=cv2.arcLength(main_contour,closed=False)
+    # else:
+    #     length_endo=0
     
-    #엔도 스켈레톤 이진화> 컨투어 추출> 길이계산
-    endo_skeleton=(endo_skeleton>0).astype(np.uint8)
-    contours,_=cv2.findContours(endo_skeleton,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    if contours:
-        main_contour=max(contours,key=lambda x:cv2.arcLength(x, False))
-        length_endo=cv2.arcLength(main_contour,closed=False)
-    else:
-        length_endo=0
+    
+    
+    # 스켈레톤 시각화 및 저장
+    # fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), sharex=True, sharey=True)
+    # ax = axes.ravel()
 
+    # ax[0].imshow(img_pil)
+    # ax[0].axis('off')
+    # ax[0].set_title('original', fontsize=20)
+
+    # ax[1].imshow(out_img)
+    # ax[1].axis('off')
+    # ax[1].set_title('Combined skeleton', fontsize=20)
+
+    # ax[1].legend(loc='upper right', fontsize=12)
+    # plt.tight_layout()
+
+    # base_name = os.path.splitext(image_name)[0]
+    # save_path = os.path.join(output_dir, f"{base_name}_allSk.png")
+    # fig.savefig(save_path)
+    # plt.close(fig)
+
+    # print(f"Saved skeleton image for {image_name} at {save_path}")
     
+    # with open('results.txt','a') as f:
+    #     for line in results:
+    #         f.write(line+'\n')
+    
+    
+    
+    #4가지 두께 재는 방법 비교 저장    
+    steps=[2,5,10]
+    img_list=[]
+    thickness_list=[]
+    for step in steps:
+        img_copy=img.copy()
+        thickness=measure_uterus_thickness(mask_endo,all_coords,img_copy,step=step,probe_half_length=200)
+        img_list.append(img_copy)
+        thickness_list.append(thickness)
+
+   
     # 시각화 및 저장
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), sharex=True, sharey=True)
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 8), sharex=True, sharey=True)
     ax = axes.ravel()
-
-    ax[0].imshow(img_pil)
+    ax[0].imshow(img)
+    ax[0].set_title('Original')
     ax[0].axis('off')
-    ax[0].set_title('original', fontsize=20)
 
-    ax[1].imshow(out_img)
-    ax[1].axis('off')
-    ax[1].set_title('Combined skeleton', fontsize=20)
-    ax[1].scatter([transition_cervix[1], transition_fundus[1]], [transition_cervix[0], transition_fundus[0]], c='red', s=5)
-    
-    # 각 클래스별 endpoint에 점 찍기
-    colors = {2: 'lime', 3: 'blue', 4: 'skyblue'}
-    for class_id in [2, 3, 4]:
-        endpoints = find_endpoints(skeletons[class_id])
-        coords = np.argwhere(endpoints)
-        if coords.size > 2:
-            coords=pick_two_farthest_points(coords)
-        ys, xs = coords[:, 0], coords[:, 1]
-        ax[1].scatter(xs, ys, s=5, facecolors=colors[class_id])
+    # 나머지 칸: step별 결과 이미지
+    for i, (img, step, thickness) in enumerate(zip(img_list, steps, thickness_list), start=1):
+        ax[i].imshow(img)
+        ax[i].set_title(f'Step={step} (thickness={thickness})')
+        ax[i].axis('off')
 
-    ax[1].legend(loc='upper right', fontsize=12)
     plt.tight_layout()
-
     base_name = os.path.splitext(image_name)[0]
-    save_path = os.path.join(output_dir, f"{base_name}_allSk.png")
+    save_path = os.path.join(output_dir, f"{base_name}_thickness.png")
     fig.savefig(save_path)
     plt.close(fig)
+
+
 
     print(f"Saved skeleton image for {image_name} at {save_path}")
     
     with open('results.txt','a') as f:
-        for line in results:
-            f.write(line+'\n')
+        for each in results:
+            f.write(each+'\n')
     
 
 
--- utils.py
+++ utils.py
@@ -10,64 +10,12 @@ from scipy.ndimage import distance_transform_edt, binary_fill_holes, gaussian_fi
 import shutil
 from pathlib import Path
 import cv2
-
-def make_axis_attention_map(mask):
-    contours,_=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cnt=max(contours,key=cv2.contourArea)
-    (cx,cy),(width,height),angle=cv2.fitEllipse(cnt)
-    #장축 방향 단위 벡터
-    theta=np.deg2rad(angle)
-    dx,dy=np.cos(theta),np.sin(theta)
-    H,W=mask.shape
-    Y,X=np.meshgrid(np.arange(H),np.arange(W),indexing='ij')
-    vec_x=X-cx
-    vec_y=Y-cy
-    #장축 방향으로 투영
-    axis_proj=vec_x*dx+vec_y*dy
-    #장축 방향에 가까울수록 값이 높
-    attention=np.exp(-(axis_proj**2)/(0.2*width)**2)
-    attention=attention*(mask>0)
-    return attention/attention.max()
-    
+from scipy.ndimage import convolve
+from skimage.draw import line
 
 
-#마스크에 내접하는 가장 큰 원 찾기 - with attention
-def find_largest_incircle_attention(mask, image_name="default", save_dir=None):
-    binary_mask = (mask > 0).astype(bool)
-    filled_mask = binary_fill_holes(binary_mask).astype(np.uint8) * 255
-    dist_map = distance_transform_edt(filled_mask)
-    attention = make_axis_attention_map(filled_mask)
-    attn_dist_map = dist_map * attention
 
-    dist_map = distance_transform_edt(filled_mask)
-    if np.max(dist_map) == 0:
-        return (0, 0), 0, filled_mask
-    
-    max_radius = np.max(dist_map)
-    max_row, max_col = np.unravel_index(np.argmax(attn_dist_map), attn_dist_map.shape)
-    center = (int(max_col), int(max_row))
-    diameter = int(2 * dist_map[max_row,max_col])
-    
-    #(마스크: 파란색, 내접원: 초록색)
-    save_path = os.path.join(save_dir, f"{image_name}.png")
-    os.makedirs(save_dir, exist_ok=True)
-    
-    result_img = np.zeros((*filled_mask.shape, 3), dtype=np.uint8)
-    result_img[filled_mask == 255] = [255, 255, 255]  # BGR 포맷
-    cv2.circle(
-        result_img,
-        center,
-        int(dist_map[max_row,max_col]),
-        (0,0,255),
-        2
-    )
-    
-    cv2.imwrite(save_path, result_img)
-    
-    return center, diameter
-
-
-#마스크에 내접하는 가장 큰 원 찾기 - w/o attention
+#마스크에 내접하는 가장 큰 원 찾기
 def find_largest_incircle(mask, image_name="default", save_dir=None):
     binary_mask = (mask > 0).astype(bool)
     filled_mask = binary_fill_holes(binary_mask).astype(np.uint8) * 255
@@ -144,3 +92,264 @@ def merge_skeletons_and_smooth(mask, smoothing_sigma=2):
     
     return smoothed_points, skeleton
     
+
+def fill_holes_opencv(mask):
+    mask = (mask > 0).astype(np.uint8) * 255
+    im_floodfill = mask.copy()
+    h, w = mask.shape[:2]
+    mask2 = np.zeros((h+2, w+2), np.uint8)
+    cv2.floodFill(im_floodfill, mask2, (0,0), 255)
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+    filled = mask | im_floodfill_inv
+    return filled
+
+def draw_major_axis(mask):
+    filled = fill_holes_opencv(mask)
+    contours, _ = cv2.findContours(filled, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if not contours:
+        return cv2.cvtColor(filled, cv2.COLOR_GRAY2BGR)
+    cnt = max(contours, key=cv2.contourArea)
+    (x, y), (width, height), angle = cv2.fitEllipse(cnt)
+    major_axis = max(width, height)
+    radius = int(major_axis / 2)
+    color_img = cv2.cvtColor(filled, cv2.COLOR_GRAY2BGR)
+    cv2.circle(color_img, (int(x), int(y)), radius, (0, 0, 255), 1)  # 빨강 원
+    return color_img
+
+def fill_mask(mask):
+
+    mask = (mask > 0).astype(np.uint8) * 255
+    mask=np.where(mask>1,1,mask)
+
+    return mask
+
+
+
+def calc_distance(pt1,pt2):
+    pt1=np.array(pt1)
+    pt2=np.array(pt2)
+    return np.linalg.norm(pt1-pt2)
+
+
+def find_endpoints(skeleton):
+    kernel = np.array([[1,1,1],
+                       [1,10,1],
+                       [1,1,1]])
+    filtered = convolve(skeleton.astype(np.uint8), kernel, mode='constant', cval=0)
+    # 중앙(10) + 이웃 1개(1) = 11인 곳이 endpoint
+    endpoints = (filtered == 11)
+    if np.sum(skeleton)==1:
+        endpoints |= skeleton.astype(bool)
+    y,x=np.where(endpoints)
+    endpoint_coords=list(zip(y,x))
+    return endpoint_coords
+
+
+#yx xy 바꾸기
+def yx2xy(pt):
+    return (pt[1],pt[0])
+
+def convert_to_int_tuple(point):
+    if isinstance(point, tuple):
+        return (int(point[0]), int(point[1]))
+    else:  # numpy array 등
+        return (int(point[0]), int(point[1]))
+
+
+#방향으로 1->0 경계점 찾기
+def extend_to_boundary(mask,point,direction,max_length=500):
+    point=np.array(point)
+    direction=np.array(direction)
+    unit_vector=direction/np.linalg.norm(direction)
+    for step in range(1,max_length):
+        extended_point=point+unit_vector*step
+        y,x=int(round(extended_point[0])),int(round(extended_point[1]))
+        if y<0 or y>=mask.shape[0] or x<0 or x>=mask.shape[1]:
+            break
+        if mask[y,x]==0:
+            return(y,x)
+    return None
+
+def get_direction_with_slope_and_uterus(start,end, mask_uterus):
+    start,end=np.array(start),np.array(end)
+    slope_vector = end - start  # start→end 방향 벡터
+
+    ys, xs = np.where(mask_uterus == 1)
+    if len(ys) == 0 or len(xs) == 0:
+        return None, None, None
+    uterus_center = np.array([np.mean(ys), np.mean(xs)])
+
+    # start, end 중 uterus_center에 더 가까운 점 선택
+    dist_start = np.linalg.norm(start - uterus_center)
+    dist_end = np.linalg.norm(end - uterus_center)
+    if dist_start < dist_end:
+        # start가 uterus_center에 더 가까우므로 start -> end(즉, slope_vector)가 uterus 방향
+        direction = slope_vector
+        base_point = end
+    else:
+        # end가 uterus_center에 더 가까우므로 end -> start(-slope_vector)가 uterus 방향
+        direction = -slope_vector
+        base_point = start
+
+    # 혹은, start/end에서 uterus_center로 직접 방향 벡터를 구할 수도 있음
+    # direction = uterus_center - base_point
+
+    return base_point, direction, uterus_center
+
+
+    
+
+
+#여러개의 점이 들어오면 가장 멀리 떨어진 두 점만 반환
+def pick_two_farthest_points(coords):
+    if len(coords) <=2 :
+        return coords
+    dists = cdist(coords,coords)
+    i,j=np.unravel_index(np.argmax(dists), dists.shape)
+    return np.array([coords[i],coords[j]])
+    
+def get_direction_for_short_skeleton(cervix_point, endo_point, mask_uterus):
+    # cervix_point와 endo_point를 numpy array로 변환
+    cervix_point = np.array(cervix_point)
+    endo_point = np.array(endo_point)
+
+    # cervix에서 endo 방향 (endo_point - cervix_point)
+    slope_vector = endo_point - cervix_point
+
+    # cervix에서 endo 반대방향 (cervix_point에서 endo_point 반대방향)
+    direction = -slope_vector
+
+    # uterus 중심 계산
+    ys, xs = np.where(mask_uterus == 1)
+    if len(ys) == 0 or len(xs) == 0:
+        return cervix_point, direction, None
+    uterus_center = np.array([np.mean(ys), np.mean(xs)])
+
+    return cervix_point, direction, uterus_center
+
+def sort_points_greedy(points, start_idx=0):
+    points = np.array(points)
+    sorted_points = [points[start_idx]]
+    remaining = np.delete(points, start_idx, axis=0)
+    for _ in range(len(points)-1):
+        last = sorted_points[-1]
+        dists = np.sum((remaining - last)**2, axis=1)
+        next_idx = np.argmin(dists)
+        sorted_points.append(remaining[next_idx])
+        remaining = np.delete(remaining, next_idx, axis=0)
+    return sorted_points
+
+def connect_points_with_skeleton_check(out_img, skeleton_image, connect_points):
+    window = 2  # 5x5 윈도우 (2칸씩 양쪽으로)
+    for i in range(len(connect_points)-1):
+        pt1 = connect_points[i]    # (y, x)
+        pt2 = connect_points[i+1]  # (y, x)
+        rr, cc = line(pt1[0], pt1[1], pt2[0], pt2[1])  # (y1, x1, y2, x2)
+        rr = np.clip(rr, 0, out_img.shape[0]-1)
+        cc = np.clip(cc, 0, out_img.shape[1]-1)
+        for r, c in zip(rr, cc):
+            r1, r2 = max(0, r-window), min(out_img.shape[0], r+window+1)
+            c1, c2 = max(0, c-window), min(out_img.shape[1], c+window+1)
+            # 주변 5x5 영역에 스켈레톤이 없으면 직선 그리기
+            if np.sum(skeleton_image[r1:r2, c1:c2]) == 0:
+                out_img[r, c] = [0, 255, 0]  # 녹색
+    return out_img
+
+def custom_connect(out_img, connect_points):
+    """
+    연결 규칙:
+    1. boundary → 4s0 직선 연결
+    2. 4s1 → 2s0 직선 연결
+    3. 2s1 → 3s0 직선 연결
+    """
+    # 연결할 포인트 쌍 정의
+    connections = [ (0,1), (2,3), (4,5) ]
+    six_connections=[ (0,1),(1,2),(3,4) ]
+    all_coords=[]
+    #cervix의 스켈레톤이 너무 짧아서 end가 1개인경우
+    conn = six_connections if len(connect_points) == 6 else connections
+    for start_idx, end_idx in conn:
+        pt1 = connect_points[start_idx]
+        pt2 = connect_points[end_idx]
+        rr, cc = line(pt1[0], pt1[1], pt2[0], pt2[1])
+        rr = np.clip(rr, 0, out_img.shape[0]-1)
+        cc = np.clip(cc, 0, out_img.shape[1]-1)
+        out_img[rr, cc] = [0, 255, 0]  # 초록색
+        all_coords.extend(list(zip(rr,cc)))
+        
+    return out_img, all_coords
+
+#각 스켈레톤의 엔드포인트 2개를 비교해서 더 cervix boundary 에 가까운걸 [0]으로, 더 먼걸 [1]로 함
+import numpy as np
+
+def reorder_endpoints_by_reference(endpoints_dict, reference_point):
+    """
+    각 스켈레톤의 양끝점 2개 중 reference_point(cervix_boundary)와의 거리를 계산하여
+    더 가까운 점을 [0], 더 먼 점을 [1]로 재정렬하는 함수
+
+    Args:
+        endpoints_dict (dict): {class_id: [(y0, x0), (y1, x1)]} 형태의 끝점 딕셔너리
+        reference_point (tuple): (y, x) 기준점
+
+    Returns:
+        dict: 재정렬된 endpoints_dict
+    """
+    reordered = {}
+    for class_id, points in endpoints_dict.items():
+        if len(points) != 2:
+            # 끝점이 2개가 아니면 그대로 유지
+            reordered[class_id] = points
+            continue
+        # 각 점과 기준점 사이의 거리 계산
+        pt0, pt1 = points
+        dist0 = np.linalg.norm(np.array(pt0) - np.array(reference_point))
+        dist1 = np.linalg.norm(np.array(pt1) - np.array(reference_point))
+        # 더 가까운 점이 [0], 더 먼 점이 [1]
+        if dist0 <= dist1:
+            reordered[class_id] = [pt0, pt1]
+        else:
+            reordered[class_id] = [pt1, pt0]
+    return reordered
+
+#직교 방향 직선 긋는 함수
+def get_orthogonal_direction(pt1, pt2):
+    dy = pt2[0] - pt1[0]
+    dx = pt2[1] - pt1[1]
+    ortho_dy = dx
+    ortho_dx = -dy
+    length = np.sqrt(ortho_dy**2 + ortho_dx**2)
+    if length > 0:
+        ortho_dy /= length
+        ortho_dx /= length
+    return ortho_dy, ortho_dx
+
+#직교하는 선이 2 영역에 얼마나 겹치는지 길이재기
+def measure_uterus_thickness(mask, all_coords, out_img, step=2, probe_half_length=30):
+    max_thickness = 0
+    max_probe_coords = None
+
+    for i in range(0, len(all_coords) - step, step):
+        pt1 = all_coords[i]
+        pt2 = all_coords[i + step]
+        ortho_dy, ortho_dx = get_orthogonal_direction(pt1, pt2)
+        center_y = int((pt1[0] + pt2[0]) / 2)
+        center_x = int((pt1[1] + pt2[1]) / 2)
+
+        probe_coords = [
+            (yy, xx)
+            for s in range(-probe_half_length, probe_half_length + 1)
+            if 0 <= (yy := int(center_y + s * ortho_dy)) < mask.shape[0]
+            and 0 <= (xx := int(center_x + s * ortho_dx)) < mask.shape[1]
+        ]
+
+        thickness = sum(mask[y, x] == 1 for y, x in probe_coords)
+        if thickness > max_thickness:
+            max_thickness = thickness
+            max_probe_coords = probe_coords
+
+    if max_probe_coords:
+        for y, x in max_probe_coords:
+            cv2.circle(out_img, (x, y), 1, (0, 255, 255), -1)  # 노란색 점
+
+    return max_thickness
+
